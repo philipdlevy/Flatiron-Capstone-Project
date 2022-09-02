9da1165ab5c72122986ef94392ec8a3ba43e9376
Philip Levy <philipdlevy@gmail.com>
2022-09-02 15:24:46 -0600

    Added controllers to backend

--- db/schema.rb
+++ db/schema.rb
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 2022_09_02_192702) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.integer "age"
     t.string "email"
     t.string "address"
