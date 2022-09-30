095b81c405c6ed058056064b61b5ee8d599b1c69
Philip Levy <philipdlevy@gmail.com>
2022-09-30 11:26:33 -0600

    Small changes for deleting gym

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -3,6 +3,6 @@ class Gym < ApplicationRecord
 
     validates :address, :phone_number, uniqueness: true, presence: true
 
-    has_many :gym_memberships
+    has_many :gym_memberships, dependent: :destroy
     has_many :users, through: :gym_memberships
 end
